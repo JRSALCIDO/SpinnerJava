80ddb23194d62e8c0e3864c926bbbc6cffde2012
JRSALCIDO <salcido.12311@gmail.com>
2023-06-05 13:26:55 -0700

    Comit Final

--- app/src/main/java/com/example/spinnerjava/MainActivity.java
+++ app/src/main/java/com/example/spinnerjava/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.spinnerjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spnPaises);
+        final String[] lista = getResources().getStringArray(R.array.paises);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, lista);
+        spinner.setAdapter(adaptador);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, lista[position], Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO: Handle the case where nothing is selected
+            }
+        });
     }
 }
